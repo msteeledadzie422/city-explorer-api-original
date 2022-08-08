ae70b546c5c39347c68e50bbf5b1133549453caf
Mandela Steele-Dadzie <msteeledadzie@gmail.com>
2022-08-07 23:59:02 -0600

    pushing code for lab08

--- server.js
+++ server.js
@@ -9,6 +9,46 @@ const axios = require('axios').default;
 
 const PORT = process.env.PORT;
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
+
+class Movies {
+  constructor(title, overview, averageVotes, totalVotes, imageUrl, popularity, releasedOn) {
+    this.title = title;
+    this.overview = overview;
+    this.average_votes = averageVotes;
+    this.total_votes = totalVotes;
+    this.image_url = imageUrl;
+    this.popularity = popularity;
+    this.released_on = releasedOn;
+  }
+
+  static getMovies(cityName, cityExplorerRes) {
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${cityName}&include_adult=false`;
+    console.log(url);
+    axios.get(url)
+      .then((response) => {
+        console.log(response.data.results);
+        const movies = this.makeMoviesList(response.data.results);
+        cityExplorerRes.send(movies);
+      })
+      .catch((error) => {
+        cityExplorerRes.sendStatus(404).send('City not found in movie data: ' + error);
+      });
+  }
+
+  static makeMoviesList(movieArray) {
+    const movies = movieArray.map((movie) => {
+      return new Movies(movie.original_title,
+        movie.overview,
+        movie.vote_average,
+        movie.vote_count,
+        'https://image.tmdb.org/t/p/w500' + movie.poster_path,
+        movie.popularity,
+        movie.release_date);
+    });
+    return movies;
+  }
+}
 
 class Forecast {
   constructor(date, description) {
@@ -25,7 +65,7 @@ class Forecast {
         cityExplorerRes.send(forecasts);
       })
       .catch((error) => {
-        response.sendStatus(404).send('City not found in weather data: ' + error);
+        cityExplorerRes.sendStatus(404).send('City not found in weather data: ' + error);
       });
   }
 
@@ -45,6 +85,11 @@ class Forecast {
 server.get('/weather', (request, response) => {
   console.log(request.query);
   Forecast.getForecasts(request.query.lat, request.query.lon, response);
+});
+
+server.get('/movies', (request, response) => {
+  console.log(request.query);
+  Movies.getMovies(request.query.city_name, response);
 });
 
 server.listen(PORT, () => {
