9ebe0a470f2c3aaf93c0e4619774bc58dbea9434
Mandela Steele-Dadzie <msteeledadzie@gmail.com>
2022-08-07 23:23:50 -0600

    code working for live weather feed

--- server.js
+++ server.js
@@ -3,16 +3,30 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const weather = require('./data/weather.json');
 const server = express();
 server.use(cors());
+const axios = require('axios').default;
 
 const PORT = process.env.PORT;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 
 class Forecast {
   constructor(date, description) {
     this.date = date;
     this.description = description;
+  }
+
+  static getForecasts(lat, lon, cityExplorerRes) {
+    lat = parseFloat(lat).toFixed(3);
+    lon = parseFloat(lon).toFixed(3);
+    axios.get(`https://api.weatherbit.io/v2.0/forecast/daily/?key=${WEATHER_API_KEY}&days=3&lat=${lat}&lon=${lon}`)
+      .then((response) => {
+        const forecasts = this.makeForecasts(response.data);
+        cityExplorerRes.send(forecasts);
+      })
+      .catch((error) => {
+        response.sendStatus(404).send('City not found in weather data: ' + error);
+      });
   }
 
   static makeForecasts(city) {
@@ -28,31 +42,9 @@ class Forecast {
   }
 }
 
-function findCity(name, lat, lon) {
-  try {
-    const city = weather.find((element) => {
-      return (
-        name === element.city_name &&
-        Math.round(lat) === Math.round(element.lat) &&
-        Math.round(lon) === Math.round(element.lon)
-      );
-    });
-    return city;
-  } catch (error) {
-    throw new Error('No City Match in Weather Data' + error);
-  }
-}
-
 server.get('/weather', (request, response) => {
   console.log(request.query);
-  const queries = request.query;
-  const city = findCity(queries.city_name, queries.lat, queries.lon);
-
-  if (city) {
-    response.status(200).send(Forecast.makeForecasts(city));
-  } else {
-    response.status(404).send('City not found in weather data');
-  }
+  Forecast.getForecasts(request.query.lat, request.query.lon, response);
 });
 
 server.listen(PORT, () => {
